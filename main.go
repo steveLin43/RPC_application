2f46bda6eb6d7559e0fc0959c0818ee41f8afbc2
ericlin <ericlin@webapiz.com>
2024-09-12 12:01:34 +0800

    針對不同協定的介面

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"RPC_application/server"
+	"flag"
 	"log"
 	"net"
+	"net/http"
 
 	pb "RPC_application/proto"
 
@@ -11,7 +13,48 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
+var grpcPort string //對接grpc、http2.0
+var httpPort string //對接http1.0
+
+func init() {
+	flag.StringVar(&grpcPort, "grpc_port", "8001", "grpc啟動通訊埠編號")
+	flag.StringVar(&httpPort, "http_port", "9001", "HTTP啟動通訊埠編號")
+	flag.Parse()
+}
+
 func main() {
+	errs := make(chan error) //創建通道以及時接收錯誤
+	go func() {
+		err := RunHttpServer(httpPort)
+		if err != nil {
+			errs <- err
+		}
+	}()
+
+	go func() { //啟動輕量級的執行緒
+		err := RunGrpcServer(grpcPort)
+		if err != nil {
+			errs <- err
+		}
+	}()
+
+	select { //等待通道來東西
+	case err := <-errs:
+		log.Fatalf("Run Server err: %v", err)
+	}
+}
+
+func RunHttpServer(port string) error { //針對http1.0
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/ping",
+		func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write([]byte(`pong`))
+		},
+	)
+	return http.ListenAndServe(":"+port, serveMux)
+}
+
+func RunGrpcServer(port string) error { //針對grpc
 	s := grpc.NewServer()
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
 	reflection.Register(s)
@@ -21,8 +64,5 @@ func main() {
 		log.Fatalf("net.Listen err: %v", err)
 	}
 
-	err = s.Serve(lis)
-	if err != nil {
-		log.Fatalf("server.Serve err: %v", err)
-	}
+	return s.Serve(lis)
 }
